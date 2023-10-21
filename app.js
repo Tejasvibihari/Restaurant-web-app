b28c3ca51b542e385175d8a9caf3ba8a306a684a
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2023-10-21 10:20:20 +0530

    form created for adding product and table created to see and update the product and also delete the product

--- app.js
+++ app.js
@@ -9,18 +9,98 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
 
 // Connect to MongoDB database
 
-// mongoose.connect("mongodb://127.0.0.1:27017/restaurentDB")
-//     .then(() => {
-//         console.log("Database Created Succesfuly");
-//     })
-//     .catch((err) => {
-//         comsole.log(err);
-//     });
+mongoose.connect("mongodb://127.0.0.1:27017/restaurentDB")
+    .then(() => {
+        console.log("Database Created Succesfuly");
+    })
+    .catch((err) => {
+        comsole.log(err);
+    });
+
+
+const pizzaSchema = new mongoose.Schema({
+    _id: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+
+});
+const Pizza = mongoose.model("Pizza", pizzaSchema);
+
+const burburSchema = new mongoose.Schema({
+    _id: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+
+});
+const Burger = mongoose.model("Burger", burburSchema);
+const beverageSchema = new mongoose.Schema({
+    _id: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+
+});
+const Beverage = mongoose.model("Beverage", beverageSchema);
+
+
+
+
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
@@ -41,9 +121,21 @@ app.get("/burgur", (req, res) => {
 app.get("/beverage", (req, res) => {
     res.render("beverage.ejs");
 });
+app.get("/addproduct", (req, res) => {
+    res.render("addproduct.ejs");
+});
+
+app.get("/product", (req, res) => {
+    res.render("product.ejs");
+});
 
 
-
+app.post("/addproduct", (req, res) => {
+    const id = req.body["id"];
+    const name = req.body["name"];
+    const description = req.body["description"];
+    const price = req.body["price"];
+})
 
 
 
