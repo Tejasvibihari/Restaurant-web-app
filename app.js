2d3e4a9ca3834dde16e96b9e16a36cb6c4280603
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2023-10-21 16:16:26 +0530

    learning

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import express from "express";
 import mongoose from "mongoose";
 import ejs from "ejs";
+import multer from "multer";
 
 const app = express();
 const port = 3000;
@@ -12,10 +13,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Image Upload
+
+// Configure Multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now + "_" + file.originalname);
+    },
+});
+
+const upload = multer({
+    storage,
+});
 
 // Connect to MongoDB database
 
-mongoose.connect("mongodb://127.0.0.1:27017/restaurentDB")
+mongoose.connect("mongodb://127.0.0.1:27017/restaurantDB")
     .then(() => {
         console.log("Database Created Succesfuly");
     })
@@ -24,7 +40,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/restaurentDB")
     });
 
 
-const pizzaSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
     _id: {
         type: Number,
         required: true,
@@ -41,62 +57,19 @@ const pizzaSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    image: {
+    type: {
+        type: String,
+        required: true
+    },
+    imagePath: {
         type: String,
         required: true
     }
 
 });
-const Pizza = mongoose.model("Pizza", pizzaSchema);
+const Product = mongoose.model("Product", productSchema);
 
-const burburSchema = new mongoose.Schema({
-    _id: {
-        type: Number,
-        required: true,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
-    }
 
-});
-const Burger = mongoose.model("Burger", burburSchema);
-const beverageSchema = new mongoose.Schema({
-    _id: {
-        type: Number,
-        required: true,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
-    }
-
-});
-const Beverage = mongoose.model("Beverage", beverageSchema);
 
 
 
@@ -130,15 +103,34 @@ app.get("/product", (req, res) => {
 });
 
 
-app.post("/addproduct", (req, res) => {
+
+
+
+app.post("/addproduct", upload.single("image"), (req, res) => {
     const id = req.body["id"];
     const name = req.body["name"];
     const description = req.body["description"];
     const price = req.body["price"];
-})
+    const type = req.body["type"];
+    const file = req.file;
 
+    if (!file) {
+        // Handle the file upload error
+        return res.status(400).send("File upload failed");
+    }
 
+    const product = new Product({
+        _id: id,
+        name: name,
+        description: description,
+        price: price,
+        type: type,
+        imagePath: `/uploads/${file.originalname}`
+    });
 
+    product.save();
+    res.redirect("/product")
+});
 
 
 
